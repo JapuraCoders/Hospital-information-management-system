862858e198e5742deed8c027e707e0e9dae0de0f
vinod <vinodsilva.10@gmail.com>
2020-12-07 00:56:03 +0530

    add gui to medicalofficer

--- src/Controller/MedicalOfficerLoginController.java
+++ src/Controller/MedicalOfficerLoginController.java
@@ -1,4 +1,33 @@
 package Controller;
 
-public class MedicalOfficerLoginController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MedicalOfficerLoginController implements Initializable{
+
+        @FXML
+        private TextField username;
+
+        @FXML
+        private PasswordField password;
+
+
+        @FXML
+        private void handleClose(MouseEvent event) {
+                System.exit(0);
+        }
+
+
+        @Override
+        public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        }
+
 }
