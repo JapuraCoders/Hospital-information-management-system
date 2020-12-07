8ac9a63a0e98b8d8afb5c2cd690028ac5357462c
ImalshaRathnayake <74772372+ImalshaRathnayake@users.noreply.github.com>
2020-12-07 15:37:53 +0530

    update receptionist interface

--- src/Controller/ReceptionistLoginController.java
+++ src/Controller/ReceptionistLoginController.java
@@ -1,4 +1,39 @@
 package Controller;
 
-public class ReceptionistLoginController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ReceptionistLoginController implements Initializable {
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private Label signupbtn;
+
+    @FXML
+    private void handleClose(MouseEvent event) {
+        System.exit(0);
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
 }
