4248fbd8f9a6e8e1d1593de8e370027f6d558593
ImalshaRathnayake <74772372+ImalshaRathnayake@users.noreply.github.com>
2020-12-13 09:59:54 +0530

    Create a postal mail abstract class

---
+++ src/Model/PostalMail.java
@@ -0,0 +1,76 @@
+package Model;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+
+public abstract class PostalMail {
+    Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+
+    protected String toName;
+    protected String fromName;
+    protected String refNo;
+    protected String address;
+    protected String note;
+    //protected document;
+    protected Date date=calendar.getTime();
+
+
+    //constructer
+    protected PostalMail(String toName,String fromName,String refNo,String address,String note,Date date){
+        this.setToName(toName);
+        this.setFromName(fromName);
+        this.setRefNo(refNo);
+        this.setAddress(address);
+        this.setNote(note);
+        this.setDate(date);
+    }
+
+
+    //setters
+    public void setToName(String toName){
+        this.toName=toName;
+    }
+    public void setFromName(String fromName){
+        this.fromName=fromName;
+    }
+    public void setRefNo(String refNo){
+        this.refNo=refNo;
+    }
+    public void setAddress(String address){
+        this.address=address;
+    }
+    public void setNote(String note){
+        this.note=note;
+    }
+    public void setDate(Date date){
+        this.date=date;
+    }
+
+
+    //getters
+    public String getToName(){
+        return this.toName;
+    }
+    public String getFromName(){
+        return this.fromName;
+    }
+    public String getRefNo(){
+        return this.refNo;
+    }
+    public String getAddress(){
+        return this.note;
+    }
+    public Date getDate(){
+        return this.date;
+    }
+
+
+
+    public String tostring(){
+        return getToName()+"\n"+getFromName()+"\n"+getRefNo()+"\n"+getAddress()+"\n"+getDate();
+    }
+
+
+}
