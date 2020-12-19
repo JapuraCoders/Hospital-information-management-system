2ca25b48dd2772655f0fdc7b6b6233fb43fbad29
ImalshaRathnayake <74772372+ImalshaRathnayake@users.noreply.github.com>
2020-12-19 16:25:40 +0530

    delete blu pane

--- src/Controller/PatientDashBoardController/PatientDashBoardController.java
+++ src/Controller/PatientDashBoardController/PatientDashBoardController.java
@@ -1,10 +1,8 @@
 package Controller.PatientDashBoardController;
 
-import com.jfoenix.controls.JFXButton;
-import javafx.fxml.FXML;
-import javafx.scene.layout.BorderPane;
+
 
 public class PatientDashBoardController {
 
-  
+
 }
