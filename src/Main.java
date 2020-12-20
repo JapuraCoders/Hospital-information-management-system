2cd223efe4f3cf45a2fe35bed55fccb1c86a30b3
vinod <vinodsilva.10@gmail.com>
2020-12-20 21:30:40 +0530

    adminDAsh board UI done

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     private static double yOffset = 0;
    @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../../Hospital-information-management-system-vinod/src/View/LoginSelect.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("View/LoginSelect.fxml"));
         primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
@@ -32,24 +32,10 @@ public class Main extends Application {
 
    }
 
-    /*public static String removeFirstAndLast(String str)
-    {   // Removing first and last character of a string using substring() method
-        str = str.substring(1, str.length() - 1);
-        return str;
-    }*/
 
     public static void main(String[] args) throws IOException {
         launch(args);
 
-        /*Login login1 = new Login("User1","password");
-        Login login2 = new Login("User2","pass");
-        LoginRecord userLogin = new LoginRecord("Files\\Details\\LoginDetails.txt");
-
-        userLogin.add(login1);
-        userLogin.add(login2);
-        userLogin.dlt("null|null|0");
-        System.out.println(userLogin.viewByID("null|null|1"));
-        System.out.println(removeFirstAndLast(userLogin.viewAllLogins().toString()));*/
 
     }
 }
